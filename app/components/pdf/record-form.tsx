827e56dcd8369b205df6b11ab6e3016efba4e84d
Khoa Cao <kqcao@wisc.edu>
2025-01-06 23:11:17 -0600

    feat: :sparkles: Almost complete pdf form for "Đơn Kính"

--- app/components/pdf/record-form.tsx
+++ app/components/pdf/record-form.tsx
@@ -1,21 +1,77 @@
 "use client";
 import dynamic from "next/dynamic";
-import { Text, View, Document, Page } from "@react-pdf/renderer";
+import { Text, View, Document, Page, Image } from "@react-pdf/renderer";
 import styles from "./style";
 
 const PDFViewer = dynamic(() => import("./pdfViewer"), {
   ssr: false
 });
 
+const phone_number = "0913963003";
+const email = "drkhanhtrang.ophth@gmail.com";
+
 export default function PagePDF() {
   const MyDocument = () => (
     <Document>
-      <Page size="A4" style={styles.page}>
-        <View style={styles.section}>
-          <Text>Section #1</Text>
+      <Page size="A5" style={styles.page}>
+        <View style={styles.header}>
+          <View>
+            <Text style={styles.textBoldItalic}>BS HUỲNH KHÁNH TRANG</Text>
+            <Text style={styles.textBoldItalic}>CHUYÊN KHOA CẤP 1 MẮT</Text>
+            <Text style={styles.textBoldItalic}>ĐTDĐ: {phone_number}</Text>
+            <Text style={styles.textBoldItalic}>EMAIL: {email}</Text>
+          </View>
+          <View>
+            <Text style={styles.textBoldItalic}>ĐỊA CHỈ: 13 Đào Duy Từ P5 Q10</Text>
+            <Text style={styles.textBoldItalic}>Giờ khám:</Text>
+            <Text style={styles.textBoldItalic}>T2-T7: 17h-19h30</Text>
+            <Text style={styles.textBoldItalic}>CN và ngày lễ: Nghỉ</Text>
+          </View>
         </View>
-        <View style={styles.section}>
-          <Text>Section #2</Text>
+        <Text style={styles.title}>ĐƠN KÍNH</Text>
+        <View style={{ marginBottom: "20px" }}>
+          <View style={styles.section}>
+            <Text style={styles.name}>Họ và tên: Cao Quang Nhật Khoa</Text>
+            <Text>Năm sinh: 2004</Text>
+            <Text>Giới tính: Nam</Text>
+          </View>
+          <Text>Địa chỉ: 13 Đào Duy Từ</Text>
+        </View>
+        <View style={styles.table}>
+          <View style={styles.tableRow}>
+            {['UCVA', 'SPH', 'CYL', 'AX', 'BCVA', 'ADD'].map((item, i) => (
+              <Text key={i} style={styles.tableColHeader}>{item}</Text>
+            ))}
+          </View>
+          {['MP/OD', 'MT/OS'].map((row, i) => (
+            <View key={i} style={styles.tableRow}>
+              <Text style={styles.tableCol}>{row}</Text>
+              {[...Array(5)].map((_, j) => (
+                <Text key={j} style={styles.tableCol}> </Text>
+              ))}
+            </View>
+          ))}
+        </View>
+        <View>
+          <Text>PD:</Text>
+          <Text>GLASSES</Text>
+          <Text>OD:</Text>
+          <Text>OS:</Text>
+        </View>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between', marginTop: '20px'}}>
+          <View>
+            <Text>Ghi chú:</Text>
+            <Text>- Hạn chế sử dụng vi tính, điện thoại</Text>
+            <Text>- Tăng cường chơi thể thao ngoài trời</Text>
+            <Text>- Đeo kính thường xuyên</Text>
+            <Text>- Tái khám sau 10 THÁNG	</Text>
+            <Text>- Đem theo toa cũ khi tái khám.</Text>
+          </View>
+          <View style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            <Text style={{textAlign: 'center'}}>Ký tên</Text>
+            <Image src={window.location.origin + "/signature.png"} cache={false} style={{width: "100%"}}/>
+            <Text style={{textAlign: 'center'}}>BS HUỲNH KHÁNH TRANG</Text>
+          </View>
         </View>
       </Page>
     </Document>
--- app/components/pdf/style.ts
+++ app/components/pdf/style.ts
@@ -1,15 +1,87 @@
-import { StyleSheet } from '@react-pdf/renderer';
+import { StyleSheet, Font } from "@react-pdf/renderer";
+
+Font.register({
+  family: "Roboto",
+  fonts: [
+    {
+      src: "https://fonts.gstatic.com/s/roboto/v32/KFOmCnqEu92Fr1Me5WZLCzYlKw.ttf"
+    },
+    {
+      src: "https://fonts.gstatic.com/s/roboto/v32/KFOlCnqEu92Fr1MmEU9vAx05IsDqlA.ttf",
+      fontWeight: "bold"
+    },
+    {
+      src: "https://fonts.gstatic.com/s/roboto/v32/KFOjCnqEu92Fr1Mu51S7ABc9AMX6lJBP.ttf",
+      fontWeight: 'bold',
+      fontStyle: 'italic',
+    }
+  ]
+});
 
 const styles = StyleSheet.create({
-    page: {
-      flexDirection: 'row',
-      backgroundColor: '#E4E4E4'
-    },
-    section: {
-      margin: 10,
-      padding: 10,
-      flexGrow: 1
-    }
+  page: {
+    fontFamily: "Roboto",
+    color: "#262626",
+    backgroundColor: '#fff',
+    fontSize: 11,
+    padding: "30px 30px"
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: "40px"
+  },
+  title: {
+    fontSize: 20,
+    textAlign: "center",
+    fontWeight: "bold",
+    marginBottom: "30px"
+  },
+  textBold: {
+    fontWeight: "bold",
+  },
+  textItalic: {
+    fontStyle: "italic",
+  },
+  textBoldItalic: {
+    fontWeight: "bold",
+    fontStyle: "italic",
+  },
+  section: {
+    flexDirection: "row",
+    gap: 10,
+    marginBottom: "20px"
+  },
+  name: {
+    flexGrow: 0.5
+  },
+  table: {
+    width: '100%',
+    marginBottom: "20px",
+    borderWidth: 1,
+    borderColor: '#000'
+  },
+  tableRow: {
+    flexDirection: 'row'
+  },
+  tableColHeader: {
+    width: '16.66%',
+    borderWidth: 1,
+    borderColor: '#000',
+    padding: 4,
+    fontWeight: 'bold'
+  },
+  tableCol: {
+    width: '16.66%',
+    borderWidth: 1,
+    borderColor: '#000',
+    padding: 4
+  },
+  signature: {
+    marginTop: 40,
+    textAlign: 'right',
+    fontWeight: 'bold'
+  }
 });
 
 export default styles
